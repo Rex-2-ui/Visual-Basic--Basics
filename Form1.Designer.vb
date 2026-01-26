d67d8be8739b2fd231bce20a3a63e962077ec76b
Rex-2-ui <rexraymond35@gmail.com>
2026-01-26 23:30:20 +0000

    Improve Form1 UI layout and add PictureBox image
    Enhanced the design and clarity of Form1:
    - Renamed GroupBox1 to "Student Information" and applied bold/hot track styling.
    - Added labels for Name, Major, and Phone; pre-filled text boxes with sample data.
    - Improved font and color styling for multiple controls.
    - Added "Output" label and formatted output in TextBox4.
    - Updated PictureBox1: new location, size, background color, and image from resources.
    - Set form background to SystemColors.Info.
    - Removed unused BackgroundWorker1 from designer and resources.
    - Adjusted GroupBox4 styling and radio button sizes.
    - Declared new label controls in the designer.
    No changes to batlogo.png.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         GroupBox1 = New GroupBox()
         TextBox1 = New TextBox()
         TextBox2 = New TextBox()
@@ -35,9 +36,12 @@ Partial Class Form1
         GroupBox4 = New GroupBox()
         RadioButton4 = New RadioButton()
         RadioButton5 = New RadioButton()
-        BackgroundWorker1 = New ComponentModel.BackgroundWorker()
         RadioButton6 = New RadioButton()
         TextBox4 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
         GroupBox1.SuspendLayout()
         GroupBox2.SuspendLayout()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
@@ -46,37 +50,44 @@ Partial Class Form1
         ' 
         ' GroupBox1
         ' 
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(Label2)
         GroupBox1.Controls.Add(TextBox2)
         GroupBox1.Controls.Add(TextBox3)
         GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         GroupBox1.ForeColor = SystemColors.HotTrack
         GroupBox1.Location = New Point(30, 25)
         GroupBox1.Name = "GroupBox1"
         GroupBox1.Size = New Size(414, 164)
         GroupBox1.TabIndex = 0
         GroupBox1.TabStop = False
-        GroupBox1.Text = "GroupBox1"
+        GroupBox1.Text = "Student Information"
         ' 
         ' TextBox1
         ' 
-        TextBox1.Location = New Point(169, 75)
+        TextBox1.Location = New Point(118, 72)
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(205, 27)
         TextBox1.TabIndex = 1
+        TextBox1.Text = "Philosophy and History"
         ' 
         ' TextBox2
         ' 
-        TextBox2.Location = New Point(169, 122)
+        TextBox2.Location = New Point(118, 127)
         TextBox2.Name = "TextBox2"
         TextBox2.Size = New Size(205, 27)
         TextBox2.TabIndex = 2
+        TextBox2.Text = "(618) 850-2424"
         ' 
         ' TextBox3
         ' 
-        TextBox3.Location = New Point(169, 26)
+        TextBox3.Location = New Point(118, 26)
         TextBox3.Name = "TextBox3"
         TextBox3.Size = New Size(205, 27)
         TextBox3.TabIndex = 3
+        TextBox3.Text = "Tom Thumb"
         ' 
         ' GroupBox2
         ' 
@@ -84,6 +95,7 @@ Partial Class Form1
         GroupBox2.Controls.Add(RadioButton3)
         GroupBox2.Controls.Add(RadioButton2)
         GroupBox2.Controls.Add(RadioButton1)
+        GroupBox2.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         GroupBox2.ForeColor = SystemColors.HotTrack
         GroupBox2.Location = New Point(512, 25)
         GroupBox2.Name = "GroupBox2"
@@ -99,7 +111,7 @@ Partial Class Form1
         RadioButton1.ForeColor = SystemColors.ControlText
         RadioButton1.Location = New Point(30, 26)
         RadioButton1.Name = "RadioButton1"
-        RadioButton1.Size = New Size(60, 24)
+        RadioButton1.Size = New Size(62, 24)
         RadioButton1.TabIndex = 0
         RadioButton1.TabStop = True
         RadioButton1.Text = "Gray"
@@ -122,7 +134,7 @@ Partial Class Form1
         RadioButton3.ForeColor = SystemColors.ControlText
         RadioButton3.Location = New Point(30, 86)
         RadioButton3.Name = "RadioButton3"
-        RadioButton3.Size = New Size(59, 24)
+        RadioButton3.Size = New Size(60, 24)
         RadioButton3.TabIndex = 2
         RadioButton3.Text = "Blue"
         RadioButton3.UseVisualStyleBackColor = True
@@ -132,27 +144,33 @@ Partial Class Form1
         CheckBox1.AutoSize = True
         CheckBox1.Checked = True
         CheckBox1.CheckState = CheckState.Checked
-        CheckBox1.Font = New Font("Segoe UI", 10F)
+        CheckBox1.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         CheckBox1.Location = New Point(136, 344)
         CheckBox1.Name = "CheckBox1"
-        CheckBox1.Size = New Size(152, 27)
+        CheckBox1.Size = New Size(153, 27)
         CheckBox1.TabIndex = 3
         CheckBox1.Text = "Message Visible"
         CheckBox1.UseVisualStyleBackColor = True
         ' 
         ' PictureBox1
         ' 
-        PictureBox1.Location = New Point(12, 407)
+        PictureBox1.BackColor = SystemColors.ControlText
+        PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), Image)
+        PictureBox1.ErrorImage = Nothing
+        PictureBox1.InitialImage = Nothing
+        PictureBox1.Location = New Point(12, 400)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(136, 84)
+        PictureBox1.Size = New Size(200, 125)
+        PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
         PictureBox1.TabIndex = 4
         PictureBox1.TabStop = False
         ' 
         ' GroupBox4
         ' 
-        GroupBox4.BackColor = SystemColors.Control
+        GroupBox4.BackColor = SystemColors.Info
         GroupBox4.Controls.Add(RadioButton5)
         GroupBox4.Controls.Add(RadioButton4)
+        GroupBox4.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         GroupBox4.ForeColor = SystemColors.HotTrack
         GroupBox4.Location = New Point(512, 229)
         GroupBox4.Name = "GroupBox4"
@@ -168,7 +186,7 @@ Partial Class Form1
         RadioButton4.ForeColor = SystemColors.Desktop
         RadioButton4.Location = New Point(30, 56)
         RadioButton4.Name = "RadioButton4"
-        RadioButton4.Size = New Size(69, 24)
+        RadioButton4.Size = New Size(70, 24)
         RadioButton4.TabIndex = 3
         RadioButton4.Text = "White"
         RadioButton4.UseVisualStyleBackColor = True
@@ -180,7 +198,7 @@ Partial Class Form1
         RadioButton5.ForeColor = SystemColors.ControlText
         RadioButton5.Location = New Point(30, 26)
         RadioButton5.Name = "RadioButton5"
-        RadioButton5.Size = New Size(65, 24)
+        RadioButton5.Size = New Size(66, 24)
         RadioButton5.TabIndex = 4
         RadioButton5.TabStop = True
         RadioButton5.Text = "Black"
@@ -193,13 +211,15 @@ Partial Class Form1
         RadioButton6.ForeColor = SystemColors.ControlText
         RadioButton6.Location = New Point(31, 125)
         RadioButton6.Name = "RadioButton6"
-        RadioButton6.Size = New Size(59, 24)
+        RadioButton6.Size = New Size(60, 24)
         RadioButton6.TabIndex = 3
         RadioButton6.Text = "Blue"
         RadioButton6.UseVisualStyleBackColor = True
         ' 
         ' TextBox4
         ' 
+        TextBox4.BackColor = SystemColors.Info
+        TextBox4.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
         TextBox4.Location = New Point(136, 229)
         TextBox4.Multiline = True
         TextBox4.Name = "TextBox4"
@@ -208,11 +228,53 @@ Partial Class Form1
         TextBox4.TabIndex = 6
         TextBox4.Text = "My Name: Tom Thumb" & vbCrLf & "Major: Philosophy and History" & vbCrLf & "Local phone: (618) 850-2424" & vbCrLf & vbCrLf
         ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = SystemColors.Desktop
+        Label1.Location = New Point(33, 75)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(55, 20)
+        Label1.TabIndex = 7
+        Label1.Text = "Major:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.ForeColor = SystemColors.ControlText
+        Label2.Location = New Point(33, 23)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(54, 20)
+        Label2.TabIndex = 8
+        Label2.Text = "Name:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.ForeColor = SystemColors.Desktop
+        Label3.Location = New Point(33, 127)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(57, 20)
+        Label3.TabIndex = 9
+        Label3.Text = "Phone:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(63, 232)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(73, 23)
+        Label4.TabIndex = 8
+        Label4.Text = "Output:"
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
         ClientSize = New Size(863, 552)
+        Controls.Add(Label4)
         Controls.Add(TextBox4)
         Controls.Add(GroupBox4)
         Controls.Add(PictureBox1)
@@ -245,8 +307,11 @@ Partial Class Form1
     Friend WithEvents GroupBox4 As GroupBox
     Friend WithEvents RadioButton5 As RadioButton
     Friend WithEvents RadioButton4 As RadioButton
-    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
     Friend WithEvents RadioButton6 As RadioButton
     Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 
 End Class
