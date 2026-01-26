ee05acefcd0250c19d33cb6ec940f08441da8340
Rex-2-ui <rexraymond35@gmail.com>
2026-01-26 22:41:46 +0000

    Add resources and update Form1 UI with new controls
    - Added Resources.resx and Resources.Designer.vb for strongly-typed resource access and localization support.
    - Removed GroupBox3 and added GroupBox4 ("Text Color") with "Black" and "White" RadioButtons.
    - Added RadioButton6 ("Blue") to GroupBox2 and relabeled other RadioButtons.
    - Set ForeColor for GroupBox1 and GroupBox2 to SystemColors.HotTrack.
    - Updated CheckBox1 to "Message Visible", set checked by default, and changed font/size.
    - Adjusted PictureBox1 location and size.
    - Added multiline TextBox4 with sample personal info.
    - Added BackgroundWorker component and related metadata.
    - Added empty event handler stubs for new controls.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -30,12 +30,18 @@ Partial Class Form1
         RadioButton1 = New RadioButton()
         RadioButton2 = New RadioButton()
         RadioButton3 = New RadioButton()
-        GroupBox3 = New GroupBox()
         CheckBox1 = New CheckBox()
         PictureBox1 = New PictureBox()
+        GroupBox4 = New GroupBox()
+        RadioButton4 = New RadioButton()
+        RadioButton5 = New RadioButton()
+        BackgroundWorker1 = New ComponentModel.BackgroundWorker()
+        RadioButton6 = New RadioButton()
+        TextBox4 = New TextBox()
         GroupBox1.SuspendLayout()
         GroupBox2.SuspendLayout()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox4.SuspendLayout()
         SuspendLayout()
         ' 
         ' GroupBox1
@@ -43,6 +49,7 @@ Partial Class Form1
         GroupBox1.Controls.Add(TextBox2)
         GroupBox1.Controls.Add(TextBox3)
         GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.ForeColor = SystemColors.HotTrack
         GroupBox1.Location = New Point(30, 25)
         GroupBox1.Name = "GroupBox1"
         GroupBox1.Size = New Size(414, 164)
@@ -73,84 +80,143 @@ Partial Class Form1
         ' 
         ' GroupBox2
         ' 
+        GroupBox2.Controls.Add(RadioButton6)
         GroupBox2.Controls.Add(RadioButton3)
         GroupBox2.Controls.Add(RadioButton2)
         GroupBox2.Controls.Add(RadioButton1)
+        GroupBox2.ForeColor = SystemColors.HotTrack
         GroupBox2.Location = New Point(512, 25)
         GroupBox2.Name = "GroupBox2"
         GroupBox2.Size = New Size(243, 164)
         GroupBox2.TabIndex = 1
         GroupBox2.TabStop = False
-        GroupBox2.Text = "GroupBox2"
+        GroupBox2.Text = "Background"
         ' 
         ' RadioButton1
         ' 
         RadioButton1.AutoSize = True
-        RadioButton1.Location = New Point(42, 27)
+        RadioButton1.Checked = True
+        RadioButton1.ForeColor = SystemColors.ControlText
+        RadioButton1.Location = New Point(30, 26)
         RadioButton1.Name = "RadioButton1"
-        RadioButton1.Size = New Size(121, 24)
+        RadioButton1.Size = New Size(60, 24)
         RadioButton1.TabIndex = 0
         RadioButton1.TabStop = True
-        RadioButton1.Text = "RadioButton1"
+        RadioButton1.Text = "Gray"
         RadioButton1.UseVisualStyleBackColor = True
         ' 
         ' RadioButton2
         ' 
         RadioButton2.AutoSize = True
-        RadioButton2.Location = New Point(42, 66)
+        RadioButton2.ForeColor = SystemColors.ControlText
+        RadioButton2.Location = New Point(30, 56)
         RadioButton2.Name = "RadioButton2"
-        RadioButton2.Size = New Size(121, 24)
+        RadioButton2.Size = New Size(56, 24)
         RadioButton2.TabIndex = 1
-        RadioButton2.TabStop = True
-        RadioButton2.Text = "RadioButton2"
+        RadioButton2.Text = "Red"
         RadioButton2.UseVisualStyleBackColor = True
         ' 
         ' RadioButton3
         ' 
         RadioButton3.AutoSize = True
-        RadioButton3.Location = New Point(42, 108)
+        RadioButton3.ForeColor = SystemColors.ControlText
+        RadioButton3.Location = New Point(30, 86)
         RadioButton3.Name = "RadioButton3"
-        RadioButton3.Size = New Size(121, 24)
+        RadioButton3.Size = New Size(59, 24)
         RadioButton3.TabIndex = 2
-        RadioButton3.TabStop = True
-        RadioButton3.Text = "RadioButton3"
+        RadioButton3.Text = "Blue"
         RadioButton3.UseVisualStyleBackColor = True
-        ' 
-        ' GroupBox3
-        ' 
-        GroupBox3.Location = New Point(157, 216)
-        GroupBox3.Name = "GroupBox3"
-        GroupBox3.Size = New Size(287, 125)
-        GroupBox3.TabIndex = 2
-        GroupBox3.TabStop = False
-        GroupBox3.Text = "GroupBox3"
         ' 
         ' CheckBox1
         ' 
         CheckBox1.AutoSize = True
-        CheckBox1.Location = New Point(157, 361)
+        CheckBox1.Checked = True
+        CheckBox1.CheckState = CheckState.Checked
+        CheckBox1.Font = New Font("Segoe UI", 10F)
+        CheckBox1.Location = New Point(136, 344)
         CheckBox1.Name = "CheckBox1"
-        CheckBox1.Size = New Size(103, 24)
+        CheckBox1.Size = New Size(152, 27)
         CheckBox1.TabIndex = 3
-        CheckBox1.Text = "CheckBox1"
+        CheckBox1.Text = "Message Visible"
         CheckBox1.UseVisualStyleBackColor = True
         ' 
         ' PictureBox1
         ' 
-        PictureBox1.Location = New Point(12, 376)
+        PictureBox1.Location = New Point(12, 407)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(125, 62)
+        PictureBox1.Size = New Size(136, 84)
         PictureBox1.TabIndex = 4
         PictureBox1.TabStop = False
+        ' 
+        ' GroupBox4
+        ' 
+        GroupBox4.BackColor = SystemColors.Control
+        GroupBox4.Controls.Add(RadioButton5)
+        GroupBox4.Controls.Add(RadioButton4)
+        GroupBox4.ForeColor = SystemColors.HotTrack
+        GroupBox4.Location = New Point(512, 229)
+        GroupBox4.Name = "GroupBox4"
+        GroupBox4.Size = New Size(250, 98)
+        GroupBox4.TabIndex = 5
+        GroupBox4.TabStop = False
+        GroupBox4.Text = "Text Color"
+        GroupBox4.UseWaitCursor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.ForeColor = SystemColors.Desktop
+        RadioButton4.Location = New Point(30, 56)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(69, 24)
+        RadioButton4.TabIndex = 3
+        RadioButton4.Text = "White"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton5
+        ' 
+        RadioButton5.AutoSize = True
+        RadioButton5.Checked = True
+        RadioButton5.ForeColor = SystemColors.ControlText
+        RadioButton5.Location = New Point(30, 26)
+        RadioButton5.Name = "RadioButton5"
+        RadioButton5.Size = New Size(65, 24)
+        RadioButton5.TabIndex = 4
+        RadioButton5.TabStop = True
+        RadioButton5.Text = "Black"
+        RadioButton5.UseVisualStyleBackColor = False
+        RadioButton5.UseWaitCursor = True
+        ' 
+        ' RadioButton6
+        ' 
+        RadioButton6.AutoSize = True
+        RadioButton6.ForeColor = SystemColors.ControlText
+        RadioButton6.Location = New Point(31, 125)
+        RadioButton6.Name = "RadioButton6"
+        RadioButton6.Size = New Size(59, 24)
+        RadioButton6.TabIndex = 3
+        RadioButton6.Text = "Blue"
+        RadioButton6.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(136, 229)
+        TextBox4.Multiline = True
+        TextBox4.Name = "TextBox4"
+        TextBox4.ScrollBars = ScrollBars.Both
+        TextBox4.Size = New Size(308, 98)
+        TextBox4.TabIndex = 6
+        TextBox4.Text = "My Name: Tom Thumb" & vbCrLf & "Major: Philosophy and History" & vbCrLf & "Local phone: (618) 850-2424" & vbCrLf & vbCrLf
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(863, 552)
+        Controls.Add(TextBox4)
+        Controls.Add(GroupBox4)
         Controls.Add(PictureBox1)
         Controls.Add(CheckBox1)
-        Controls.Add(GroupBox3)
         Controls.Add(GroupBox2)
         Controls.Add(GroupBox1)
         Name = "Form1"
@@ -160,6 +226,8 @@ Partial Class Form1
         GroupBox2.ResumeLayout(False)
         GroupBox2.PerformLayout()
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox4.ResumeLayout(False)
+        GroupBox4.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -172,8 +240,13 @@ Partial Class Form1
     Friend WithEvents RadioButton3 As RadioButton
     Friend WithEvents RadioButton2 As RadioButton
     Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents GroupBox3 As GroupBox
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents TextBox4 As TextBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -2,4 +2,16 @@
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
 
     End Sub
+
+    Private Sub GroupBox4_Enter(sender As Object, e As EventArgs) Handles GroupBox4.Enter
+
+    End Sub
+
+    Private Sub RadioButton5_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton5.CheckedChanged
+
+    End Sub
+
+    Private Sub RadioButton6_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton6.CheckedChanged
+
+    End Sub
 End Class
