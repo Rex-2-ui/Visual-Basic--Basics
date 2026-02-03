06aeeac7a6cc939e390f097c4edbdca7e705565f
Rex-2-ui <rexraymond35@gmail.com>
2026-02-03 01:14:54 +0000

    VB project two

---
+++ VBProjectTwo/Form1.Designer.vb
@@ -0,0 +1,265 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        TextBox7 = New TextBox()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        TextBox6 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = SystemColors.Control
+        GroupBox1.Controls.Add(TextBox7)
+        GroupBox1.Controls.Add(Label7)
+        GroupBox1.Controls.Add(Label6)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(TextBox6)
+        GroupBox1.Controls.Add(TextBox5)
+        GroupBox1.Controls.Add(TextBox4)
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox1.ForeColor = SystemColors.MenuHighlight
+        GroupBox1.Location = New Point(24, 30)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(701, 386)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Textbook Sale Information"
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(261, 343)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(125, 30)
+        TextBox7.TabIndex = 13
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.ForeColor = SystemColors.InactiveCaptionText
+        Label7.Location = New Point(50, 343)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(82, 23)
+        Label7.TabIndex = 12
+        Label7.Text = "Due Total"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.ForeColor = SystemColors.InfoText
+        Label6.Location = New Point(51, 295)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(78, 23)
+        Label6.TabIndex = 11
+        Label6.Text = "Sales Tax"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.ForeColor = SystemColors.InactiveCaptionText
+        Label5.Location = New Point(50, 245)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(74, 23)
+        Label5.TabIndex = 10
+        Label5.Text = "Subtotal"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.ForeColor = SystemColors.InfoText
+        Label4.Location = New Point(50, 198)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(77, 23)
+        Label4.TabIndex = 9
+        Label4.Text = "Quantity"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.ForeColor = SystemColors.InfoText
+        Label3.Location = New Point(50, 154)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(47, 23)
+        Label3.TabIndex = 8
+        Label3.Text = "Price"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.ForeColor = SystemColors.InfoText
+        Label2.Location = New Point(50, 106)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(133, 23)
+        Label2.TabIndex = 7
+        Label2.Text = "ISBN (Identifier)"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.ForeColor = SystemColors.InactiveCaptionText
+        Label1.Location = New Point(50, 56)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 23)
+        Label1.TabIndex = 6
+        Label1.Text = "Book Title"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(261, 147)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(125, 30)
+        TextBox6.TabIndex = 5
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(261, 292)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(125, 30)
+        TextBox5.TabIndex = 4
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(261, 242)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 30)
+        TextBox4.TabIndex = 3
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(261, 198)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 30)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(261, 99)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(209, 30)
+        TextBox2.TabIndex = 1
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(261, 49)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(310, 30)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(234, 444)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 1
+        Button1.Text = "Compute"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(337, 444)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 2
+        Button2.Text = "Reset "
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(437, 444)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 3
+        Button3.Text = "Totals"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(550, 444)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 4
+        Button4.Text = "Exit"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 508)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox7 As TextBox
+
+End Class
---
+++ VBProjectTwo/Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.Common
+
+Public Class Form1
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim BookTitleString As String
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        Dim ISBNString As String
+    End Sub
+End Class
