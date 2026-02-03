d9a8b2dc5ed200eadcd5d3ca4810653b3a87ee34
Rex-2-ui <rexraymond35@gmail.com>
2026-02-03 01:03:56 +0000

    adding events to buttond

--- Form1.vb
+++ Form1.vb
@@ -24,4 +24,23 @@ Public Class Form1
     Private Sub DisplayButton_Popup(sender As Object, e As PopupEventArgs)
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox4.Text = "Name: " & TextBox3.Text & vbCrLf &
+                            "Major: " & TextBox1.Text & vbCrLf &
+                            "Phone Number: " & TextBox2.Text & vbCrLf
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox4.Text = ""
+        TextBox3.Text = ""
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
 End Class
