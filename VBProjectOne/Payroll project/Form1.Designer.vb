3397564bc0f5da85c5f1d32f5b623eddfa94edfb
Rex-2-ui <rexraymond35@gmail.com>
2026-02-10 08:54:51 +0000

    payroll project

---
+++ VBProjectOne/Payroll project/Form1.Designer.vb
@@ -0,0 +1,488 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        GroupBox3 = New GroupBox()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        TextBox10 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        GroupBox4 = New GroupBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        CheckBox5 = New CheckBox()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        GroupBox4.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Controls.Add(CheckBox1)
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox1.Location = New Point(22, 24)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(733, 185)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Used Automative Information"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(RadioButton4)
+        GroupBox2.Controls.Add(RadioButton3)
+        GroupBox2.Controls.Add(RadioButton2)
+        GroupBox2.Controls.Add(RadioButton1)
+        GroupBox2.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox2.Location = New Point(465, 236)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(290, 181)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Exterior Extras"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(Label10)
+        GroupBox3.Controls.Add(Label9)
+        GroupBox3.Controls.Add(Label8)
+        GroupBox3.Controls.Add(Label7)
+        GroupBox3.Controls.Add(Label6)
+        GroupBox3.Controls.Add(Label5)
+        GroupBox3.Controls.Add(Label4)
+        GroupBox3.Controls.Add(TextBox10)
+        GroupBox3.Controls.Add(TextBox9)
+        GroupBox3.Controls.Add(TextBox8)
+        GroupBox3.Controls.Add(TextBox7)
+        GroupBox3.Controls.Add(TextBox6)
+        GroupBox3.Controls.Add(TextBox5)
+        GroupBox3.Controls.Add(TextBox4)
+        GroupBox3.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        GroupBox3.Location = New Point(22, 236)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(361, 368)
+        GroupBox3.TabIndex = 1
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Sales Information"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(222, 26)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(184, 30)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(542, 26)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 30)
+        TextBox2.TabIndex = 1
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(222, 87)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(445, 30)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(129, 37)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 30)
+        TextBox4.TabIndex = 1
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(129, 87)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(125, 30)
+        TextBox5.TabIndex = 2
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(129, 136)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(125, 30)
+        TextBox6.TabIndex = 3
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(129, 186)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(125, 30)
+        TextBox7.TabIndex = 4
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(129, 236)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(125, 30)
+        TextBox8.TabIndex = 5
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.Location = New Point(129, 288)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(125, 30)
+        TextBox9.TabIndex = 6
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(129, 335)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(125, 30)
+        TextBox10.TabIndex = 7
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(244, 659)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 2
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(394, 659)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 3
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(543, 659)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 4
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(688, 659)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 5
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox4
+        ' 
+        GroupBox4.Controls.Add(CheckBox5)
+        GroupBox4.Controls.Add(CheckBox4)
+        GroupBox4.Controls.Add(CheckBox3)
+        GroupBox4.Controls.Add(CheckBox2)
+        GroupBox4.Location = New Point(465, 429)
+        GroupBox4.Name = "GroupBox4"
+        GroupBox4.Size = New Size(290, 196)
+        GroupBox4.TabIndex = 6
+        GroupBox4.TabStop = False
+        GroupBox4.Text = "Accessory Extra"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(36, 33)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(102, 23)
+        Label1.TabIndex = 3
+        Label1.Text = "Lot Number"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(434, 33)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(42, 23)
+        Label2.TabIndex = 4
+        Label2.Text = "Year"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(36, 94)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(168, 23)
+        Label3.TabIndex = 5
+        Label3.Text = "Vehicle Make/Model"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(17, 44)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(47, 23)
+        Label4.TabIndex = 8
+        Label4.Text = "Price"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(17, 94)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(77, 23)
+        Label5.TabIndex = 9
+        Label5.Text = "Discount"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(17, 143)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(56, 23)
+        Label6.TabIndex = 10
+        Label6.Text = "Extras"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(17, 193)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(74, 23)
+        Label7.TabIndex = 11
+        Label7.Text = "Subtotal"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(17, 243)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(78, 23)
+        Label8.TabIndex = 12
+        Label8.Text = "Sales Tax"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(17, 295)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(73, 23)
+        Label9.TabIndex = 13
+        Label9.Text = "Trade-in"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(17, 338)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(80, 23)
+        Label10.TabIndex = 14
+        Label10.Text = "Total due"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(49, 40)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(68, 24)
+        RadioButton1.TabIndex = 0
+        RadioButton1.Text = "None"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Checked = True
+        RadioButton2.Location = New Point(49, 70)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(136, 24)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Paint Touch-Up"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(49, 109)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(102, 24)
+        RadioButton3.TabIndex = 2
+        RadioButton3.Text = "Undercoat"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(49, 139)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(62, 24)
+        RadioButton4.TabIndex = 3
+        RadioButton4.Text = "Both"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(222, 139)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(189, 27)
+        CheckBox1.TabIndex = 0
+        CheckBox1.Text = "Whole Sale Discount"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(26, 26)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(103, 24)
+        CheckBox2.TabIndex = 1
+        CheckBox2.Text = "CheckBox2"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(26, 72)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(103, 24)
+        CheckBox3.TabIndex = 2
+        CheckBox3.Text = "CheckBox3"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(26, 113)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(103, 24)
+        CheckBox4.TabIndex = 3
+        CheckBox4.Text = "CheckBox4"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox5
+        ' 
+        CheckBox5.AutoSize = True
+        CheckBox5.Location = New Point(26, 151)
+        CheckBox5.Name = "CheckBox5"
+        CheckBox5.Size = New Size(103, 24)
+        CheckBox5.TabIndex = 4
+        CheckBox5.Text = "CheckBox5"
+        CheckBox5.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(840, 720)
+        Controls.Add(GroupBox4)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        GroupBox4.ResumeLayout(False)
+        GroupBox4.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+
+End Class
