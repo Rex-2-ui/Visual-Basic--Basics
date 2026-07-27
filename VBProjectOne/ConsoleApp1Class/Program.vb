1fa2e029fcef15a204f45d242c0d888a0d78ce3d
Rex-2-ui <rexraymond35@gmail.com>
2026-07-27 13:40:51 +0000

    #1

---
+++ VBProjectOne/ConsoleApp1Class/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.Write("GRRR POOOWWW POOWWW")
+        Console.WriteLine("5I am Batman Who are you:")
+
+
+        Dim Name As String = Console.ReadLine()
+    End Sub
+End Module
