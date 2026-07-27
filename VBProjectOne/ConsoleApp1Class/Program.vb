1a02167030a7214347af2102137f3b0dabe82339
Rex-2-ui <rexraymond35@gmail.com>
2026-07-27 14:00:47 +0000

    #2

--- VBProjectOne/ConsoleApp1Class/Program.vb
+++ VBProjectOne/ConsoleApp1Class/Program.vb
@@ -2,11 +2,28 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        Console.Write("GRRR POOOWWW POOWWW")
-        Console.WriteLine("5I am Batman Who are you:")
+
+        ' Console.WriteLine("Hello World!")
+        ' Console.Write("GRRR POOOWWW POOWWW")
+        ' Console.WriteLine("I am Batman Who are you:")
 
 
-        Dim Name As String = Console.ReadLine()
+        ' Dim Name As String = Console.ReadLine()
+        'Console.WriteLine("Hello" & Name)
+
+        Dim Num1, Num2 As Double
+        Console.WriteLine("Enter First Number")
+        Num1 = Console.ReadLine()
+
+        Console.WriteLine("Enter Second Number")
+        Num2 = Console.ReadLine()
+
+        Dim Sum As Double = Num1 + Num2
+        Console.WriteLine("The sum of " & Num1 & Num2 & "is " & Sum)
+
+
+        Dim Name As String
+        Dim Age As
+
     End Sub
 End Module
